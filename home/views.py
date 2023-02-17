6f51cc43401a0556e9c7773963a055e04cd6f630
JoyZadan <joyzadan.learning@gmail.com>
2023-02-17 03:19:22 +0000

    added ngos views and templates

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from ngos.models import Category
 
 
 def index(request):
     """ A view to return the index page """
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
     return render(request, 'home/index.html')
--- ngos/views.py
+++ ngos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Category, NonGovernmentOrg
 
-# Create your views here.
+
+def all_ngos(request):
+    """
+    A view to show all the NGOs, including
+    sorting and search queries
+    """
+
+    nongovernmentorgs = NonGovernmentOrg.objects.all()
+
+    context = {
+        'nongovernmentorgs': nongovernmentorgs,
+    }
+
+    return render(request, 'ngos/ngos.html', context)
