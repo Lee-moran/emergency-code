794e07aabc491a81eb7e0c5e13068941c9d90e9a
JoyZadan <joyzadan.learning@gmail.com>
2023-02-18 15:32:19 +0000

    added category or causes and NGOs filtering and sorting
    added ngo detail views, urls and template

--- ngos/urls.py
+++ ngos/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_ngos, name='ngos'),
+    path('<int:nongovernmentorg_id>/', views.ngo_detail, name='ngo_detail'),
 ]
